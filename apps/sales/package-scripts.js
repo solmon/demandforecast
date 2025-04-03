b8f079d7ed1cd80e1783bf25b55981fcff87d1db
solmon <solmon@gmail.com>
2025-04-04 00:21:08 +0530

    setup of the basic app

---
+++ apps/sales/package-scripts.js
@@ -0,0 +1,16 @@
+const { crossEnv } = require('@gowravshekar/nps-utils');
+
+const mainScripts = require('../../scripts/nps/next.package-scripts')({
+  packageName: '@app/sales',
+  devPort: 3001,
+  prodPort: 3001,
+});
+
+module.exports = {
+  scripts: {
+    ...mainScripts.scripts,
+    build: {
+      default: crossEnv('next build'),
+    },
+  },
+};
